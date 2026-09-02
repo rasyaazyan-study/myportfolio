6f58fc29bcb56cd1f83e2a6ba0921ddb49244974
rasyaazyan-study <rasya.azyan@ui.ac.id>
2026-09-02 17:28:10 +0700

    fix: db credentials

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -85,11 +85,11 @@ if PRODUCTION:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('rasya.azyan'),
-            'USER': os.getenv('rasya.azyan'),
-            'PASSWORD': os.getenv('o7SOrMt3'),
-            'HOST': os.getenv('10.119.106.139'),
-            'PORT': os.getenv('5432'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
             'OPTIONS': {
                 'options': f"-c search_path={os.getenv('SCHEMA', 'public')}"
             }
