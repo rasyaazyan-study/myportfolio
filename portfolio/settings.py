1da4932c6111f59232b3385ee9fdafcb28430cf8
rasyaazyan-study <rasya.azyan@ui.ac.id>
2026-09-09 23:44:32 +0700

    update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -14,10 +14,16 @@ from pathlib import Path
 
 import os
 from dotenv import load_dotenv
+
+APP_ENV = os.getenv("APP_ENV", "local")
 #Load environment variables from .env file
 load_dotenv()
 
+if APP_ENV == "prod":
+    load_dotenv(".env.prod", override=True)
+
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rasya-azyan-myportfolio.pws.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["https://rasya-azyan-myportfolio.pws.cs.ui.ac.id",]
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 
@@ -32,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bz#^t&^z$ng18by1^z1_fp@jd+k+4g98xd4j(!yk%0$hjg3nbi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = not PRODUCTION
 
 
 # Application definition
