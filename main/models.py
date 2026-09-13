097666f689c5222bf12b9203d5513dd7a6cb9efc
rasyaazyan-study <rasya.azyan@ui.ac.id>
2026-09-13 23:21:04 +0700

    build: education page & models

--- main/models.py
+++ main/models.py
@@ -29,3 +29,27 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+    
+#Tugas Individu 2
+class Education(models.Model):
+    EDUCATION_CHOICES = [
+        ('SD', 'Sekolah Dasar'),
+        ('SMP', 'Sekolah Menengah Pertama'),
+        ('SMA', 'Sekolah Menengah Atas'),
+        ('S1', 'Program Sarjana')
+    ]
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    institution = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField()
+    category = models.CharField(max_length=20, choices=EDUCATION_CHOICES)
+    thumbnail = models.URLField(blank=True, null=True)
+    started_at = models.DateTimeField()
+    ended_at = models.DateTimeField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.institution
+        
+    @property
+    def is_ongoing(self):
+        return self.ended_at is None
--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 from main.models import Experience
+from main.models import Education
 
 
 def show_main(request):
@@ -25,3 +26,10 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_education(request):
+    context = {
+        "name": "Rasya Azyan Kautsar",
+        "education_list": Education.objects.all(),
+    }
+    return render(request, "education.html", context)
